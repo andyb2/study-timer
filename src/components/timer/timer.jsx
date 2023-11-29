2b59fa8fff3993eec1517051c45e7f3821ece3e7
Andrew Boyle <a.michael.boyle@gmail.com>
2023-11-29 12:42:45 -0500

    timer folder created to hold timer jsx and css module

---
+++ src/components/timer/timer.jsx
@@ -0,0 +1,97 @@
+import styles from './timer.module.css';
+import { useState, useRef, useEffect } from 'react';
+
+export default function Timer() {
+  const [timer, setTimer] = useState({
+    minuteOne: 0,
+    minuteTwo: 0,
+    secondOne: 0,
+    secondTwo: 0,
+  });
+  const [timeLimit, setTimeLimit] = useState(0);
+  //   const [breakTime, setBreakTime] = useState(0);
+  const timerRef = useRef(null);
+  //   const breakTimerRef = useRef(null);
+
+  const handleSetTime = (time) => {
+    const toNum = Number(time);
+    setTimeLimit(toNum);
+  };
+
+  const handleStartTimer = () => {
+    if (timeLimit) {
+      timerRef.current = setInterval(() => {
+        setTimer((prev) => {
+          const stateCopy = { ...prev };
+
+          if (prev.minuteTwo === 9) {
+            stateCopy.minuteOne += 1;
+            stateCopy.minuteTwo = 0;
+            return stateCopy;
+          }
+
+          if (prev.secondOne === 5 && prev.secondTwo === 9) {
+            stateCopy.minuteTwo += 1;
+            stateCopy.secondOne = 0;
+            stateCopy.secondTwo = 0;
+            return stateCopy;
+          }
+
+          if (prev.secondTwo === 9) {
+            stateCopy.secondTwo = 0;
+            stateCopy.secondOne += 1;
+            return stateCopy;
+          }
+
+          stateCopy.secondTwo += 1;
+          return stateCopy;
+        });
+      }, 10);
+    }
+  };
+
+  const handleStopTimer = () => {
+    clearInterval(timerRef.current);
+  };
+
+  useEffect(() => {
+    const checkForFinishTime = () => {
+      const { minuteOne, minuteTwo } = timer;
+      let totalMin = Number(minuteOne.toString() + minuteTwo.toString());
+
+      if (timeLimit && totalMin === timeLimit) {
+        //     breakTimerRef.current = setInterval(() => {});
+        handleStopTimer();
+      }
+    };
+
+    checkForFinishTime();
+  }, [timer, timeLimit]);
+
+  return (
+    <div className={styles.container}>
+      <select onChange={(e) => handleSetTime(e.target.value)}>
+        <option default>Select Time Limit</option>
+        <option value={5}>5 min</option>
+        <option value={10}>10 min</option>
+        <option value={15}>15 min</option>
+        <option value={20}>20 min</option>
+      </select>
+
+      <div className={styles.timer}>
+        {timer.minuteOne}
+        {timer.minuteTwo}:{timer.secondOne}
+        {timer.secondTwo}
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <button onClick={() => handleStartTimer()} className={styles.button}>
+          Start
+        </button>
+        <button onClick={() => handleStopTimer()} className={styles.button}>
+          Stop
+        </button>
+      </div>
+    </div>
+  );
+}
